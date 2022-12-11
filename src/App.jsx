c19b938117ab14499d621e3c9273c117f66e7c15
devluisapaim <luisac.paim@gmail.com>
2022-12-11 17:32:37 -0300

    add task hidden

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ function App() {
     <>
       <Header />
       <Entrada />
-      <Menu />
       <Task />
       <Footer />
     </>
--- src/components/Entrada.jsx
+++ src/components/Entrada.jsx
@@ -1,11 +1,30 @@
 import imgPlus from "../assets/circled-plus.svg";
 import styles from "../styles/entrada.module.css";
+import { useEffect, useState } from "react";
+
 
 export const Entrada = () => {
+    const [status, setStatus] = useState('block');
+
+    function handleStatus() {
+        const hidden = 'none';
+        const shown = 'block';
+
+        status === shown ? setStatus(hidden) : setStatus(shown);
+    }
+
     return(
-        <div className={styles.entrada__container}>
-            <input className={styles.entrada__input} type="text" />
-            <button className={styles.entrada__inputButton}><img className={styles.entrada__inputImg} src={imgPlus} alt="" /></button>
+        <>
+        <div className={styles.teste__container}>
+            <img onClick={handleStatus} className={styles.teste} src={imgPlus} alt="" />
         </div>
+        <div className={styles.container} style={{display: `${status}`}}>
+            <h3 className={styles.entrada__title}>Adicionar tarefa</h3>
+            <div className={styles.entrada__container}>
+                <input className={styles.entrada__input} type="text" />
+                <button className={styles.entrada__inputButton}><img className={styles.entrada__inputImg} src={imgPlus} alt="" /></button>
+            </div>
+        </div>
+        </>
     );
 };
